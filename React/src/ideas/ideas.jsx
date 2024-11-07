4b95c803ae5504ae8e6697b7e8bcb578dcfb3fbc
TayziaS <taycat21@gmail.com>
2024-11-06 21:07:36 -0700

    adding view components components

---
+++ React/src/ideas/ideas.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Ideas() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>ideas displayed here</div>
+    </main>
+  );
+}
---
+++ React/src/review/review.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Review() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>review displayed here</div>
+    </main>
+  );
+}
