e609efe77055f8a7263924d425aa8724e100e45c
TayziaS <taycat21@gmail.com>
2024-11-07 22:30:24 -0700

    fixed goals CSS & Bootstrap Accordions

--- React/src/goals/goals.jsx
+++ React/src/goals/goals.jsx
@@ -1,123 +1,63 @@
 import React from 'react';
-import './goals.css';
+import { Accordion, Button, Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS
+import './goals.css'; // Your custom CSS
+import { Link } from 'react-router-dom';
 
 export function Goals() {
-  return (
+    return (
     <main>
-        <container>
+      <div className="container">
+        {/* Dropdown for creating a new goal */}
+        <div className="container" id="goalButton">
+            <Accordion>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>New Goal</Accordion.Header>
+                    <Accordion.Body>
+                        <a className="dropdown-item" href="plan">Make New</a>
+                        <a className="dropdown-item" href="ideas">Brainstorm Idea</a>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
+        </div>
 
-            <div className="dropdown">
-                <button onclick="myFunction()" className="dropbtn btn btn-primary">New Goal</button>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="goals">myGoals</a>
-                    <a className="dropdown-item" href="plan">myGoalsetter</a>
-                    <a className="dropdown-item" href="ideas">Brainstrom</a>
-                </div>
-            </div>
+        {/* Active Goals Section */}
 
-            <div className="active">
-                <h2>Active Goals</h2>
+        <div className="container" id="active">
+        <h2>Active Goals</h2>
+            <Accordion>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>Goal 1</Accordion.Header>
+                    <Accordion.Body>
+                    <p className="goalDesc">Lorem ipsum...</p>
+                  <p className="goalDate">mm/dd/yyyy</p>
+                  <p className="reviewDate">review set for: mm/dd/yyyy</p>
+                  <button className='myButton'><a href="/plan">Edit Goal</a></button>
+                  <button className='myButton'><a href="/review">Review Goal</a></button>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
+            {/* Repeat accordion items for additional goals */}
+        </div>
 
-                <div className="accordion" id="accordionExample">
-                    <div className="accordion-item">
-                    <h2 className="accordion-header" id="headingOne">
-                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    
-                        </button>
-                    </h2>
-                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                        <div className="accordion-body">
-                            <p className="goalDesc">Lorem ipsum...</p> 
-                            <p className="goalDate">mm/dd/yyyy</p> 
-                            <p className="reviewDate">review set for: mm/dd/yyyy</p>
-                            <button><a href="plan">Edit Goal</a></button>
-                            <button><a href="review">Review Goal</a></button>
-                        </div>
-                    </div>
-                    </div>
-                    <div className="accordion-item">
-                        <h2 className="accordion-header" id="headingTwo">
-                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                            Goal 2 
-                        </button>
-                        </h2>
-                        <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                            <div className="accordion-body">
-                                <p className="goalDesc">Lorem ipsum...</p> 
-                                <p className="goalDate">mm/dd/yyyy</p> 
-                                <p className="reviewDate">review set for: mm/dd/yyyy</p> 
-                                <button><a to="plan">Edit Goal</a></button>
-                                <button><a to="review">Review Goal</a></button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="accordion-item">
-                        <h2 className="accordion-header" id="headingThree">
-                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                            Goal 3
-                        </button>
-                        </h2>
-                        <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                            <div className="accordion-body">
-                                <p className="goalDesc">Lorem ipsum...</p> 
-                                <p className="goalDate">mm/dd/yyyy</p> 
-                                <p className="reviewDate">review set for: mm/dd/yyyy</p> 
-                                <button><a href="plan.html">Edit Goal</a></button>
-                                <button><a href="review.html">Review Goal</a></button>
-                            </div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
+        {/* Past Goals Section */}
+        <div className="container" id="past">
+        <h2>Past Goals</h2>
+            <Accordion>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>Goal a</Accordion.Header>
+                    <Accordion.Body>
+                    <p className="goalDesc">Lorem ipsum...</p>
+                    <p className="goalDate">mm/dd/yyyy</p>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
+            {/* Repeat accordion items for additional goals */}
+        </div>
+      </div>
 
-            
-            <div className="past">
-                <h2>Past Goals</h2>
-                <div className="accordion" id="accordionExample">
-                    <div className="accordion-item">
-                    <h2 className="accordion-header" id="headingOne">
-                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        Goal a 
-                        </button>
-                    </h2>
-                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                        <div className="accordion-body">
-                            <p className="goalDesc">Lorem ipsum...</p> 
-                            <p className="goalDate">mm/dd/yyyy</p> 
-                        </div>
-                    </div>
-                    </div>
-                    <div className="accordion-item">
-                        <h2 className="accordion-header" id="headingTwo">
-                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                            Goal b 
-                        </button>
-                        </h2>
-                        <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                            <div className="accordion-body">
-                                <p className="goalDesc">Lorem ipsum...</p> 
-                                <p className="goalDate">mm/dd/yyyy</p> 
-                            </div>
-                        </div>
-                    </div>
-                    <div className="accordion-item">
-                        <h2 className="accordion-header" id="headingThree">
-                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                            Goal c 
-                        </button>
-                        </h2>
-                        <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                            <div className="accordion-body">
-                                <p className="goalDesc">Lorem ipsum...</p> 
-                                <p className="goalDate">mm/dd/yyyy</p> 
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </container>
-
-        <aside>
+      {/* Aside for achievements/logs */}
+      <aside>
             <div className="container-fluid text-center shoutouts">
                 <h1>Achievements Log</h1> 
             </div>
