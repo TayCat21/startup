d4eafa2ce16b7e2a11f536e3cbf56754a38f4f11
TayziaS <taycat21@gmail.com>
2024-11-08 18:53:21 -0700

    finally got modal to display

--- React/src/ideas/popup.jsx
+++ React/src/ideas/popup.jsx
@@ -1,30 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './modal.css';
 
 const Modal = () => {
     const [isOpen, setIsOpen] = useState(false);
-  
+
     const toggleModal = () => {
-      setIsOpen(!isOpen);
+        setIsOpen(prevState => !prevState);
     };
-  
+
+    useEffect(() => {
+        console.log("Modal isOpen state:", isOpen);
+    }, [isOpen]);
+
     return (
-      <div>
-        <button onClick={toggleModal}>Open Modal</button>
-  
-        {isOpen && (
-          <div className="modal-overlay">
-            <div className="modal">
-              <h2>Modal Content</h2>
-              <p>Great Job on your Brainstorming session! If there is an idea you love, 
-                 drag it to the box below and we'll set it into a goal!
-              </p>
-              <button onClick={toggleModal}>Close</button>
-            </div>
-          </div>
-        )}
-      </div>
+        <div>
+            <button onClick={toggleModal}>Open Modal</button>
+
+            {isOpen && (
+                <div className="modal-overlay">
+                    <div
+                        className="modal"
+                        style={{ display: 'block' }} // Force display to block
+                    >
+                        <h2>Modal Content</h2>
+                        <p>Great Job on your Brainstorming session! If there is an idea you love, 
+                            drag it to the box below and we'll set it into a goal!
+                        </p>
+                        <button onClick={toggleModal}>Close</button>
+                    </div>
+                </div>
+            )}
+        </div>
     );
-  };
-  
-  export default Modal;
+};
+
+export default Modal;
